ab9dcfdb764a1ed512aa88dd285355651d91ba71
renatommiguel <renatommiguel@gmail.com>
2026-03-13 12:01:28 +0100

    wip

--- media_organizer/utils.py
+++ media_organizer/utils.py
@@ -4,7 +4,8 @@ import logging
 
 logger = logging.getLogger("media_organizer")
 
-IMAGE_EXTENSIONS = frozenset({".jpg", ".jpeg", ".png", ".heic"})
+IMAGE_EXTENSIONS = frozenset({".jpg", ".jpeg", ".png", ".heic", ".gif", ".webp", ".bmp", ".pdf", ".tiff", ".tif"})
 RAW_EXTENSIONS = frozenset({".cr2", ".cr3", ".nef", ".arw", ".dng", ".raf", ".rw2", ".orf"})
-VIDEO_EXTENSIONS = frozenset({".mp4", ".mov", ".avi", ".mkv", ".m4v"})
-SUPPORTED_EXTENSIONS = IMAGE_EXTENSIONS | RAW_EXTENSIONS | VIDEO_EXTENSIONS
+VIDEO_EXTENSIONS = frozenset({".mp4", ".mpg", ".mov", ".avi", ".mkv", ".m4v", ".3gp", ".wmv"})
+AUDIO_EXTENSIONS = frozenset({".mp3", ".wav", ".flac", ".aac", ".ogg", ".wma", ".m4a"})
+SUPPORTED_EXTENSIONS = IMAGE_EXTENSIONS | RAW_EXTENSIONS | VIDEO_EXTENSIONS | AUDIO_EXTENSIONS
